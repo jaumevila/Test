2ba06dfb6a4eb043c4211f96d7998cc10d5c68f4
JaumeVila <jaume.vila@uvic.cat>
2014-02-20 18:05:15 +0100

    Initial commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by ios_Uvic on 20/02/14.
+//  Copyright (c) 2014 ios_Uvic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by ios_Uvic on 20/02/14.
+//  Copyright (c) 2014 ios_Uvic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
